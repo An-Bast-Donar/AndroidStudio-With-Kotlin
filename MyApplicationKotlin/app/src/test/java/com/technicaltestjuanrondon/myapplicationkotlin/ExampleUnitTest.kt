a801e45b5bf2ae23d73e5e13700cfd3ca16df8e6
An Bast <sebastianrondonja@hotmail.com>
2023-02-21 01:34:13 -0500

    Pruebas unitarias

--- MyApplicationKotlin/app/src/test/java/com/technicaltestjuanrondon/myapplicationkotlin/ExampleUnitTest.kt
+++ MyApplicationKotlin/app/src/test/java/com/technicaltestjuanrondon/myapplicationkotlin/ExampleUnitTest.kt
@@ -14,4 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun testAddition() {
+        val sum = 2 + 2
+        assertEquals(4, sum)
+    }
 }
